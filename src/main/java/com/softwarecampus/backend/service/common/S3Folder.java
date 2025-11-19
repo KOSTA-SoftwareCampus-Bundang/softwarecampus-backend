119e229ea8a5b882ce5228aabbb5fb04672f8e51
Jaehyun Park <earlydreamer@naver.com>
2025-11-19 18:14:15 +0900

    주석 위치 조정

--- src/main/java/com/softwarecampus/backend/service/common/S3Folder.java
+++ src/main/java/com/softwarecampus/backend/service/common/S3Folder.java
@@ -1,4 +1,9 @@
- /**
+package com.softwarecampus.backend.service.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
  * S3 폴더 타입 정의
  * 허용된 S3 폴더 경로를 타입 안전하게 관리합니다.
  *
@@ -9,11 +14,6 @@
  * s3Service.uploadFile(file, S3Folder.COURSE.getPath());
  * </pre>
  */
-package com.softwarecampus.backend.service.common;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 @Getter
 @RequiredArgsConstructor
 public enum S3Folder {
