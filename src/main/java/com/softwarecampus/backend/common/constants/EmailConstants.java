46ecabe0dc2de0406a358da3948c1abfcc0db412
taeyun <afds119@gmail.com>
2025-11-30 19:55:57 +0900

    switch 표현식 + 템플릿

--- src/main/java/com/softwarecampus/backend/common/constants/EmailConstants.java
+++ src/main/java/com/softwarecampus/backend/common/constants/EmailConstants.java
@@ -4,30 +4,31 @@ package com.softwarecampus.backend.common.constants;
  * 이메일 관련 상수
  */
 public final class EmailConstants {
-    
+
     private EmailConstants() {
         throw new AssertionError("상수 클래스는 인스턴스화할 수 없습니다");
     }
-    
+
     // 이메일 발신자 정보
     public static final String SENDER_EMAIL = "noreply@softwarecampus.com";
     public static final String SENDER_NAME = "소프트웨어캠퍼스";
-    
+
     // 인증 코드 설정
     public static final int CODE_LENGTH = 6;
     public static final int CODE_MIN = 0;
     public static final int CODE_MAX = 999999;
-    
+
     // 만료 시간
     public static final int EXPIRY_MINUTES = 3;
     public static final int EXPIRY_SECONDS = EXPIRY_MINUTES * 60; // 180초
-    
+
     // 보안 설정
     public static final int MAX_ATTEMPTS = 5;
     public static final int BLOCK_DURATION_MINUTES = 30;
     public static final int RESEND_COOLDOWN_SECONDS = 60;
-    
+
     // 이메일 제목
     public static final String SUBJECT_SIGNUP = "[소프트웨어캠퍼스] 회원가입 인증 코드";
     public static final String SUBJECT_PASSWORD_RESET = "[소프트웨어캠퍼스] 비밀번호 재설정 인증 코드";
+    public static final String SUBJECT_PASSWORD_CHANGE = "[소프트웨어캠퍼스] 비밀번호 변경 인증 코드";
 }
--- src/main/java/com/softwarecampus/backend/domain/common/VerificationType.java
+++ src/main/java/com/softwarecampus/backend/domain/common/VerificationType.java
@@ -5,14 +5,15 @@ package com.softwarecampus.backend.domain.common;
  */
 public enum VerificationType {
     SIGNUP("회원가입"),
-    PASSWORD_RESET("비밀번호 재설정");
-    
+    PASSWORD_RESET("비밀번호 재설정"),
+    PASSWORD_CHANGE("비밀번호 변경");
+
     private final String description;
-    
+
     VerificationType(String description) {
         this.description = description;
     }
-    
+
     public String getDescription() {
         return description;
     }
