2a320986ae58b16a6ec94163c371b02ca5ab4f68
Jaehyun Park <earlydreamer@naver.com>
2025-12-01 16:32:58 +0900

    refactor: CourseFavorite 엔티티 및 로직을 Hard Delete 방식으로 변경

--- src/main/java/com/softwarecampus/backend/domain/course/CourseFavorite.java
+++ src/main/java/com/softwarecampus/backend/domain/course/CourseFavorite.java
@@ -1,33 +1,30 @@
 package com.softwarecampus.backend.domain.course;
 
-import com.softwarecampus.backend.domain.common.BaseSoftDeleteSupportEntity;
+import com.softwarecampus.backend.domain.common.BaseTimeEntity;
 import com.softwarecampus.backend.domain.user.Account;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Entity
-@Table(name = "course_favorite",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"account_id", "course_id"})
-        })
+@Table(name = "course_favorite", uniqueConstraints = {
+                @UniqueConstraint(columnNames = { "account_id", "course_id" })
+})
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CourseFavorite extends BaseSoftDeleteSupportEntity {
+public class CourseFavorite extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "account_id", nullable = false)
-    private Account account;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "account_id", nullable = false)
+        private Account account;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "course_id", nullable = false)
-    private Course course;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "course_id", nullable = false)
+        private Course course;
 }
