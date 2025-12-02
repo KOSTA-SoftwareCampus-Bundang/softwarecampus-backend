be0ab5e210f0ef4343018216b8a7b0fe98b27fc0
Jaehyun Park <earlydreamer@naver.com>
2025-12-02 14:17:41 +0900

    fix: @Slf4j 어노테이션 추가

--- src/main/java/com/softwarecampus/backend/service/course/CourseReviewServiceImpl.java
+++ src/main/java/com/softwarecampus/backend/service/course/CourseReviewServiceImpl.java
@@ -14,11 +14,13 @@ import com.softwarecampus.backend.repository.user.AccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
